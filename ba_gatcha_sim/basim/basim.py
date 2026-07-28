ed5eea43e3145997d8c5e0ce863d6f0745b0f5fa
mizma <omoikane@path-works.net>
2026-07-28 20:15:13 +0900

    format code

--- ba_gatcha_sim/basim/basim.py
+++ ba_gatcha_sim/basim/basim.py
@@ -10,6 +10,7 @@ from pprint import pformat
 import click
 import random
 
+
 class Level(IntEnum):
     NOTSET = 0
     DEBUG = 10
@@ -17,6 +18,7 @@ class Level(IntEnum):
     WARNING = 30
     ERROR = 40
     CRITICAL = 50
+
 
 def pout(msg=None, Verbose=0, level=Level.INFO, newline=True):
     """stdout support method
@@ -34,45 +36,47 @@ def pout(msg=None, Verbose=0, level=Level.INFO, newline=True):
         level (Level) -- Set message level for coloring (default: {Level.INFO})
         newline (bool) -- set to False if trailing new line is not needed (default: {True})
     """
-    error=False
+    error = False
     if level in {Level.NOTSET, Level.DEBUG}:
         # blah
         if Verbose < 2:
             return
-        fg = 'magenta'
+        fg = "magenta"
     elif level == Level.INFO:
-        fg = 'green'
+        fg = "green"
     elif level == Level.WARNING:
         if Verbose < 1:
             return
-        fg = 'yellow'
-        error=True
+        fg = "yellow"
+        error = True
     elif level in {Level.ERROR, Level.CRITICAL}:
-        fg = 'red'
-        error=True
+        fg = "red"
+        error = True
     else:
-        fg = 'white'
+        fg = "white"
         pass
     click.echo(click.style(str(msg), fg=fg), nl=newline, err=error)
 
+
 def old_pull(count, Verbose=0):
     if count >= 199:
-        return True # TODO: この場合200連目でピックアップ排出が0.7%のほうで出た場合、１体目ならそこで終了できる事が考慮できていない。
+        return True  # TODO: この場合200連目でピックアップ排出が0.7%のほうで出た場合、１体目ならそこで終了できる事が考慮できていない。
     elif random.random() < 0.007:
         return True
     else:
         return False
 
+
 def old_sim(target=2, cycles=10000, reset=False, rcount=2, Verbose=0):
     pout("Starting Old Gatcha Simulation")
-    pout(f"target={target}, cycles={cycles}", Verbose, Level.DEBUG )
-    MAX_PULLS = (target * 200)
+    pout(f"target={target}, cycles={cycles}", Verbose, Level.DEBUG)
+    MAX_PULLS = target * 200
 
     histogram = [0] * (MAX_PULLS + 1)
     cumulative = [0] * (MAX_PULLS + 1)
     for _ in range(cycles):
-        pulls   = 0 # initialize current pull count
-        p_cnt     = 0 # initialize the pitty count
+        pulls = 0  # initialize current pull count
+        p_cnt = 0  # initialize the pitty count
         # run simulation for cycles times
 
         for pull in range(MAX_PULLS):
@@ -83,14 +87,14 @@ def old_sim(target=2, cycles=10000, reset=False, rcount=2, Verbose=0):
                 if reset and pulls == rcount:
                     p_cnt = 0
                 if pulls == target:
-                    histogram[pull+1] += 1
+                    histogram[pull + 1] += 1
                     break
             else:
                 p_cnt += 1
             if p_cnt == 200:
                 p_cnt = 0
         pass
-    pout(f"OLD: {histogram}",Verbose,Level.DEBUG)
+    pout(f"OLD: {histogram}", Verbose, Level.DEBUG)
     cumul = 0
     for i in range(MAX_PULLS + 1):
         cumul += histogram[i]
@@ -102,6 +106,7 @@ def old_sim(target=2, cycles=10000, reset=False, rcount=2, Verbose=0):
     results["cumul"] = cumulative
     return results
 
+
 def new_pull(count, Verbose=0):
     if count >= 199:
         return True
@@ -112,31 +117,34 @@ def new_pull(count, Verbose=0):
     else:
         return False
 
-def new_sim(target=2, cycles=10000, count=0, reset=False, rcount=2, use_mile=False, Verbose=0):
+
+def new_sim(
+    target=2, cycles=10000, count=0, reset=False, rcount=2, use_mile=False, Verbose=0
+):
     pout("Starting New Gatcha Simulation")
-    pout(f"target={target}, cycles={cycles}, count={count}", Verbose, Level.DEBUG )
-    MAX_PULLS = (target * 200)
+    pout(f"target={target}, cycles={cycles}, count={count}", Verbose, Level.DEBUG)
+    MAX_PULLS = target * 200
 
     histogram = [0] * (MAX_PULLS + 1)
     cumulative = [0] * (MAX_PULLS + 1)
     for _ in range(cycles):
-        pulls   = 0 # initialize current pull count
-        p_cnt     = count # initialize the pitty count
+        pulls = 0  # initialize current pull count
+        p_cnt = count  # initialize the pitty count
         tickets = 0
         tickets_used = 0
         mileage = 0
         if use_mile:
-            mileage_bonus = {
-                70, 130, 150, 170,
-                270, 330, 350, 370
-            }
+            mileage_bonus = {70, 130, 150, 170, 270, 330, 350, 370}
         else:
             mileage_bonus = {}
         # run simulation for cycles times
 
         for pull in range(MAX_PULLS):
-            pout(f"[{pull}] : pulls={pulls} p_cnt={p_cnt} tickets={tickets} utickets={tickets_used} mile={mileage}",
-                 Verbose, Level.DEBUG )
+            pout(
+                f"[{pull}] : pulls={pulls} p_cnt={p_cnt} tickets={tickets} utickets={tickets_used} mile={mileage}",
+                Verbose,
+                Level.DEBUG,
+            )
             if tickets > 0:
                 success = False
                 for _ in range(tickets):
@@ -148,29 +156,32 @@ def new_sim(target=2, cycles=10000, count=0, reset=False, rcount=2, use_mile=Fal
                         success = True
                     else:
                         p_cnt += 1
-                pout(f"[{pull}] : TICKET pulls={pulls} p_cnt={p_cnt} tickets={tickets} utickets={tickets_used} mile={mileage}",
-                    Verbose, Level.DEBUG )
+                pout(
+                    f"[{pull}] : TICKET pulls={pulls} p_cnt={p_cnt} tickets={tickets} utickets={tickets_used} mile={mileage}",
+                    Verbose,
+                    Level.DEBUG,
+                )
                 if success:
                     pulls += 1
                     if reset and pulls == rcount:
                         mileage = 0
                     if pulls == target:
-                        pout(f"RECORD: {pull+tickets_used}", Verbose, Level.DEBUG)
+                        pout(f"RECORD: {pull + tickets_used}", Verbose, Level.DEBUG)
                         histogram[pull] += 1
                         break
 
             if new_pull(p_cnt, Verbose):
-                pout(f"Success at pull={pull+1}, p_cnt={p_cnt}", Verbose, Level.DEBUG)
-                p_cnt   = 0     # Reset pitty count every successful pull
+                pout(f"Success at pull={pull + 1}, p_cnt={p_cnt}", Verbose, Level.DEBUG)
+                p_cnt = 0  # Reset pitty count every successful pull
                 mileage += 1
                 if mileage in mileage_bonus:
                     tickets += 10
-                pulls   += 1
+                pulls += 1
                 if reset and pulls == rcount:
                     mileage = 0
                 if pulls == target:
-                    pout(f"RECORD: {pull+1+tickets_used}", Verbose, Level.DEBUG)
-                    histogram[pull+1] += 1
+                    pout(f"RECORD: {pull + 1 + tickets_used}", Verbose, Level.DEBUG)
+                    histogram[pull + 1] += 1
                     break
             else:
                 p_cnt += 1
@@ -178,7 +189,7 @@ def new_sim(target=2, cycles=10000, count=0, reset=False, rcount=2, use_mile=Fal
                 if mileage in mileage_bonus:
                     tickets += 10
         pass
-    pout(f"NEW: {histogram}",Verbose,Level.DEBUG)
+    pout(f"NEW: {histogram}", Verbose, Level.DEBUG)
     cumul = 0
     for i in range(MAX_PULLS + 1):
         cumul += histogram[i]
@@ -190,6 +201,7 @@ def new_sim(target=2, cycles=10000, count=0, reset=False, rcount=2, use_mile=Fal
     results["cumul"] = cumulative
     return results
 
+
 def cmd(kwargs):
     """BlueArchive gatcha monte-carlo simulation
     Implementation.
@@ -199,19 +211,18 @@ def cmd(kwargs):
     """
     verbose = kwargs["verbose"]
     pout("Command line arguments:", verbose, Level.INFO)
-    pout(pformat(kwargs,depth=3,indent=4), verbose, Level.INFO)
-
+    pout(pformat(kwargs, depth=3, indent=4), verbose, Level.INFO)
 
     # 1. Now parse kwargs
-    count   = kwargs['count']
-    cycles  = kwargs['cycles']
-    run_new = kwargs['new']
-    run_old = kwargs['old']
-    target  = kwargs['target']
-    reset   = kwargs['reset']
-    rcount  = kwargs['reset_count']
-    use_mile = kwargs['mileage']
-    Verbose = kwargs['verbose']
+    count = kwargs["count"]
+    cycles = kwargs["cycles"]
+    run_new = kwargs["new"]
+    run_old = kwargs["old"]
+    target = kwargs["target"]
+    reset = kwargs["reset"]
+    rcount = kwargs["reset_count"]
+    use_mile = kwargs["mileage"]
+    Verbose = kwargs["verbose"]
 
     # 2. and do it's bidding
     if run_new:
@@ -220,17 +231,23 @@ def cmd(kwargs):
     if run_old:
         old_res = old_sim(target, cycles, reset, rcount, Verbose)
 
-    MAX_PULLS = (target * 200)
+    MAX_PULLS = target * 200
     if run_new and run_old:
         print("PULLS,NEW HIST,NEW CUMUL(%),OLD HIST,OLD CUMUL(%)")
         for i in range(MAX_PULLS + 1):
-            print(f"{i},{(new_res['hist'][i]/cycles)*100},{(new_res['cumul'][i]/cycles)*100},{(old_res['hist'][i]/cycles)*100},{(old_res['cumul'][i]/cycles)*100}")
+            print(
+                f"{i},{(new_res['hist'][i] / cycles) * 100},{(new_res['cumul'][i] / cycles) * 100},{(old_res['hist'][i] / cycles) * 100},{(old_res['cumul'][i] / cycles) * 100}"
+            )
     elif run_new:
         print("PULLS,NEW HIST,NEW CUMUL(%)")
         for i in range(MAX_PULLS + 1):
-            print(f"{i},{(new_res['hist'][i]/cycles)*100},{(new_res['cumul'][i]/cycles)*100}")
+            print(
+                f"{i},{(new_res['hist'][i] / cycles) * 100},{(new_res['cumul'][i] / cycles) * 100}"
+            )
     elif run_old:
         print("PULLS,OLD HIST,OLD CUMUL(%)")
         for i in range(MAX_PULLS + 1):
-            print(f"{i},{(old_res['hist'][i]/cycles)*100},{(old_res['cumul'][i]/cycles)*100}")
+            print(
+                f"{i},{(old_res['hist'][i] / cycles) * 100},{(old_res['cumul'][i] / cycles) * 100}"
+            )
     pass
--- ba_gatcha_sim/cli.py
+++ ba_gatcha_sim/cli.py
@@ -9,64 +9,71 @@ from __future__ import absolute_import, division, print_function
 
 # Import the main click library
 import click
+
 # Import the sub-command implementations
 from .basim import basim
+
 # Import the version information
 from ba_gatcha_sim._version import __version__
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
 @click.command()
+@click.option("--old", "-o", is_flag=True, help="run old gatcha sim")
 @click.option(
-    '--old', '-o', is_flag=True,
-    help='run old gatcha sim'
-    )
+    "--reset",
+    "-r",
+    is_flag=True,
+    help="reset old count and mileage after <rcnt> PU pull",
+)
 @click.option(
-    '--reset', '-r', is_flag=True,
-    help='reset old count and mileage after <rcnt> PU pull'
-    )
+    "--reset-count",
+    "-R",
+    default=2,
+    type=int,
+    metavar="<rcnt>",
+    help="reset old count and mileage after <rcnt> PU pull",
+)
+@click.option("--mileage", "-m", is_flag=True, help="use mileage tickets")
+@click.option("--new", "-n", is_flag=True, help="run new gatcha sim")
 @click.option(
-    '--reset-count', '-R', default=2, type=int,
-    metavar='<rcnt>',
-    help='reset old count and mileage after <rcnt> PU pull'
-    )
+    "--target",
+    "-t",
+    default=2,
+    type=int,
+    metavar="<tgt>",
+    help="simulate up to target pickup count",
+)
 @click.option(
-    '--mileage', '-m', is_flag=True,
-    help='use mileage tickets'
-    )
+    "--cycles",
+    "-c",
+    default=10000,
+    type=int,
+    metavar="<cyc>",
+    help="number of cycles to simulate",
+)
 @click.option(
-    '--new', '-n', is_flag=True,
-    help='run new gatcha sim'
-    )
-@click.option(
-    '--target', '-t', default=2, type=int,
-    metavar='<tgt>',
-    help='simulate up to target pickup count'
-    )
-@click.option(
-    '--cycles', '-c', default=10000, type=int,
-    metavar='<cyc>',
-    help='number of cycles to simulate'
-    )
-@click.option(
-    '--count', '-C', default=0, type=int,
-    metavar='<cnt>',
-    help='initial count for new gatcha'
-    )
-@click.option(
-    '--verbose', '-v', count=True,
-    help='output in verbose mode'
-    )
+    "--count",
+    "-C",
+    default=0,
+    type=int,
+    metavar="<cnt>",
+    help="initial count for new gatcha",
+)
+@click.option("--verbose", "-v", count=True, help="output in verbose mode")
 @click.version_option(version=__version__)
 def cli(**kwargs):
-    """BlueArchive gatcha monte-carlo simulation
-    """
+    """BlueArchive gatcha monte-carlo simulation"""
     basim.cmd(kwargs)
     pass
+
 
 # Entry point
 def main():
     """Main script."""
     cli()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
